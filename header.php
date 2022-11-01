bdad9e8fcacc79f539771893af70aa19c88a04a8
newbiecode123456 <117134782+newbiecode123456@users.noreply.github.com>
2022-11-01 10:41:23 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Trang chủ - UEH COMMUNITY SHOP</title>
+  </head>
+  <body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	
+	
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+	  <a class="navbar-brand" href="#"><img src="img/logo.png" width=200px;></a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse flex-row-reverse" id="collapsibleNavbar">
+		<ul class="navbar-nav">
+		  <li class="nav-item">
+			<a class="nav-link" href="index.php" style="font-size:28px;color:#ffffff;">Trang chủ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="product.php" style="font-size:28px;color:#ffffff;">Sản phẩm</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="about.html" style="font-size:28px;color:#ffffff;">Giới thiệu</a>
+		  </li>
+		</ul>
+	  </div>
+	  
+	</nav>
+	
+	<div class="container-fluid" style="background-color:#33a7b5;">
+	
+	<!--header enddddd-->
